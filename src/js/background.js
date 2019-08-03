86aa20e86995ec47a540c16c0ad72d59dcd5cb70
MarcMogdanz <info@minekayama.de>
2019-08-03 17:51:58 +0200

    update/add icons

---
+++ src/js/background.js
@@ -0,0 +1,7 @@
+// needs to be loaded for webpack
+import "../img/icon-16.png";
+import "../img/icon-32.png";
+import "../img/icon-96.png";
+import "../img/icon-128.png";
+import "../img/icon-196.png";
+import "../img/icon.png";
